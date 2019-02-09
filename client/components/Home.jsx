3c7f098ca4268cfb4503138ae922f7f108d15e46
Katene Jack Cotton <katenejack@gmail.com>
2019-02-09 18:19:21 +1300

    mapping home page working

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -6,10 +6,11 @@ class Home extends React.Component {
     constructor(props){
         super(props)
         this.state = {
-            food: null
+            food: []
         }
         this.getFood = this.getFood.bind(this)
     }
+
 
     componentDidMount(){
         this.getFood()
@@ -22,29 +23,19 @@ class Home extends React.Component {
         })
     }
 
-    render(){
 
+
+
+    render(){
         return(
             <div className="row justify-content-center">
             <ul className="list-unstyled">
-                <li>
-                    <h3>Leroy Burger</h3>
-                    <img src="images/food1.png" alt="food"/>
-                    <p>Standard beef burger with cheese.</p>
-                    <p><strong>Rate: </strong>4</p>
-                </li>
-                <li>
-                    <h3>Chicken Katsu Donburi</h3>
-                    <img src="images/food2.jpg" alt="food"/>
-                    <p>Standard katsu chicken donburi with noodles.</p>
-                    <p><strong>Rate: </strong>5</p>
-                </li>
-                <li>
-                    <h3>Big Papa Burger</h3>
-                    <img src="images/food3.png" alt="food"/>
-                    <p>Fried chicken, bacon, beefy beef patty, cheese, lettuce, tomato, aioli.</p>
-                    <p><strong>Rate: </strong>2</p>
-                </li>
+                {this.state.food.map((food,i) => (<li key={i}>
+                    <h3>{food.name}</h3>
+                    <img src={`images/${food.image}`} alt={food.name}/>
+                    <p>{food.description}</p>
+                    <p><strong>Rate: </strong>{food.rate}</p>
+                </li>))}
             </ul>
         </div>
         )
--- server/routes/route.js
+++ server/routes/route.js
@@ -5,7 +5,6 @@ const router = express.Router()
 router.use(express.json())
 
 router.get('/', (req,res)=> {
-    console.log('kiaora')
     db.getFood()
     .then(food => {
         return res.json(food)
