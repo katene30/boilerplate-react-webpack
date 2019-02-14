452e82b741e31fbfcc186b876e4d350403c82267
Katene Jack Cotton <katenejack@gmail.com>
2019-02-15 09:31:18 +1300

    making a badge

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -30,11 +30,12 @@ class Home extends React.Component {
         return(
             <div className="row justify-content-center">
             <ul className="list-unstyled">
-                {this.state.food.map((food,i) => (<Link className="link" to={`/food/${food.id}`} key={i} style={{ textDecoration: 'none' }}><li>
+                {this.state.food.map((food,i) => (<Link className="link" to={`/food/${food.id}`} key={i} style={{ textDecoration: 'none' }}>
+                <li className="list-group-item justify-content-between align-items-center">
                     <h3>{food.name}</h3>
                     <img src={`images/${food.image}`} alt={food.name}/>
                     <p>{food.description}</p>
-                    <p><strong>Rate: </strong>{food.rate}</p>
+                    <p><strong>Rate: </strong>{food.rate}<span class="badge badge-primary badge-pill">14</span></p>
                 </li>
                 </Link>
                 ))}
